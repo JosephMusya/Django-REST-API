ec050e54e72afa8362509aa51dd6b3ff4f1aef36
Musya Joseph <57936020+JosephMusya@users.noreply.github.com>
2022-04-20 17:19:18 +0300

    Update serializers.py
    Added user serializer class

--- serializers.py
+++ serializers.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
 from .models import Event,Message
+from django.contrib.auth.models import User
 class EventSerializer(serializers.ModelSerializer):
+    host_name = serializers.ReadOnlyField(source='host.username')
+    host_id = serializers.ReadOnlyField(source='host.id')
+    host_email = serializers.ReadOnlyField(source='host.email')
+    
     class Meta:
-        model = Event
-        fields = ['host','topic','location','about','date','pic','created','updated']
+        model = Event       
+        #fields = ['host','topic','location','about','date','pic','created','updated']
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    Event = serializers.PrimaryKeyRelatedField(many=True,
+                                                 queryset=Event.objects.all())
+    class Meta:
+        model = User
+        #fields = ['id','username','Event']
+        fields = '__all__'
